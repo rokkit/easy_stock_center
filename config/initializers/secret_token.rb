f062a5a96e773aa2cdb349733d14675bb590fb6b
Maxim Indykov <indmaksim@gmail.com>
2013-10-18 15:33:30 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EasyStockCenter::Application.config.secret_key_base = '3f3f2794b9688888f18609eba9dea21fb3988d5e27e0431b7d2f5f573b3583746702f7268614c309e60bb0219ff0a63a04b7d64192cf0ae75201196c9b2c5011'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EasyStockCenter::Application.config.session_store :cookie_store, key: '_easy_stock_center_session'
